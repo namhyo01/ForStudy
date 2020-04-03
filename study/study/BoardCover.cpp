7c72e1aa239ad7bb004627c0cf339abc5e37551a
namhyo01 <namhyo01@naver.com>
2020-04-03 23:29:14 +0900

    commit

--- study/study/BoardCover.cpp
+++ study/study/BoardCover.cpp
@@ -5,17 +5,63 @@
 #include <stdlib.h>
 #include <string>
 #include <string.h>
+#include <vector>
 #pragma warning(disable:4996)
 using namespace std;
 //주어진 칸을 덮을 수 있는 모든 경우의 수
 // 우리는 항상 주어진 칸의 맨 왼쪽중에서 맨 윗부터 채우면 된다.
-const int coverType[4][3][2] = { //(y,x)모양
+const int coverType[4][3][2] = { //(y,x)모양 // const로 상수화를 시켜 수정 불가능 생성
 	{{0,0},{1,0},{0,1}},
 	{{0,0},{0,1},{1,1}},
 	{{0,0},{1,0},{1,1}},
 	{{0,0},{1,0},{1,-1}}
-}
+};
+// board의 (y,x)를 type번 방법으로 덮거나 덮었던 블록을 없앤다
+// delta = 1이면 덮고, -1이면 없앤다
 
+bool set(vector<vector<int> > & board, int y, int x, int type, int delta) {
+	bool ok = true;
+	for (int i = 0; i < 3; i++) {
+		const int ny = y + coverType[type][i][0];
+		const int nx = x + coverType[type][i][1];
+		if (ny < 0 || ny >= board.size() || nx < 0 || nx >= board.size()) {
+			return ok = false;
+		}
+		else if ((board[ny][nx] += delta) > 1) { // 중복 적용
+			return ok = false;
+		}
+	}
+	return ok;
+}
+// board의 모든 블럭을 다 감쌀 경우의 수
+// board가 1이면 이미 덮인것 아니면 안덮인것
+int cover(vector<vector<int> >& board)
+{
+	//아직 채우지 않은 칸중, 가장 위고 왼쪽에 있는 것을 고를 것이다.
+	int y = -1, x = -1;
+	for (int i = 0; i < board.size(); i++) {
+		for (int j = 0; j < board[i].size(); j++) {
+			if (board[i][j] == 0) {
+				y = i;
+				x = j;
+				break;
+
+			}
+		}
+		if (y != -1) break;
+	}
+	//모든 칸을 채웠으면 1을 반환
+	if (y == -1) return 1;
+	int ret = 0;
+	for (int type = 0; type < 4; type++) {
+		if (set(board, y, x, type, 1)) {
+			ret += cover(board);
+
+		}
+	}
+
+
+}
 int H, W;
 int main() {
 	int C;

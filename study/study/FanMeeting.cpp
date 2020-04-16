5d9e7ea92452df2a308a77cbfb6571212080cbd7
namhyo01 <namhyo01@naver.com>
2020-04-16 23:40:46 +0900

    c

--- study/study/FanMeeting.cpp
+++ study/study/FanMeeting.cpp
@@ -13,7 +13,7 @@ vector<int> multiply(vector<int> &a, vector<int> &b) {
 	vector<int> c(a.size() + b.size() + 1, 0);
 	for (int i = 0; i < a.size(); i++) {
 		for (int j = 0; j < b.size(); j++) {
-			c[i + j] += a[i] * b[j];
+			c[i + j] += (a[i] * b[j]);
 		}
 	}
 	return c;
@@ -21,17 +21,17 @@ vector<int> multiply(vector<int> &a, vector<int> &b) {
 void addTo(vector<int> &a, vector<int> &b, int k) {
 	int s = max(b.size() + k, a.size());
 	a.resize(s, 0);
-	b.resize(s, 0);
-	for (int i = 0; i < s - k; i++) {
+	//b.resize(s, 0);
+	for (int i = 0; i < b.size(); i++) {
 		a[i + k] += b[i];
 	}
 }
 
 void subFrom(vector<int> &a, vector<int>&b) {
-	int s = max(b.size(), a.size());
+	int s = max(b.size()+1, a.size());
 	a.resize(s, 0);
-	b.resize(s, 0);
-	for (int i = 0; i < s; i++) a[i] -= b[i];
+	//b.resize(s, 0);
+	for (int i = 0; i < b.size(); i++) a[i] -= b[i];
 }
 vector<int> karatsuba(vector<int> &a, vector<int> &b) {
 	int an = a.size(), bn = b.size();
@@ -43,8 +43,8 @@ vector<int> karatsuba(vector<int> &a, vector<int> &b) {
 	//a와 b를 밑에서 half 자리와 나머지로 분리한다.
 	vector<int> a0(a.begin(), a.begin() + half);
 	vector<int> a1(a.begin() + half, a.end());
-	vector<int> b0(b.begin(), b.begin() + min<int>(half, b.size()));
-	vector<int> b1(b.begin() + min<int>(half, b.size()), b.end());
+	vector<int> b0(b.begin(), b.begin() + min<int>(half, bn));
+	vector<int> b1(b.begin() + min<int>(half, bn), b.end());
 	//z2 = a1*b1
 	vector<int> z2 = karatsuba(a1, b1);
 	//z0 = a0*b0
@@ -58,8 +58,8 @@ vector<int> karatsuba(vector<int> &a, vector<int> &b) {
 	//ret = z0+z1 * 10^half + z2*10^(half*2)
 	vector<int> ret;
 	addTo(ret, z0, 0);
-	addTo(ret, z1, 10);
-	addTo(ret, z2, half * 2);
+	addTo(ret, z1, half);
+	addTo(ret, z2, half + half);
 	
 	return ret;
 }
@@ -86,6 +86,11 @@ int main() {
 	int C;
 	cin >> C;
 	for (int k = 0; k < C; k++) {
+		string members;
+		string fans;
+		cin >> members;
+		cin >> fans;
+		cout << hugs(members, fans)<<endl;
 
 	}
 

aaddb947947420a8628a00e4173c83516f04f035
namhyo01 <namhyo01@naver.com>
2020-07-20 21:31:55 +0900

    c

---
+++ study/study/다각형대각선.cpp
@@ -0,0 +1,19 @@
+//백준 https://www.acmicpc.net/problem/3049
+
+#include <iostream>
+#include <stdio.h>
+#include <math.h>
+using namespace std;
+// 사실상 4개의 꼭짓점에서 만드는 변들의 교차점이니 n개의 꼭짓점에서 4개의 변을 찾는 조합 nC4이다
+int main()
+{
+	int n;
+	cin >> n;
+	if (n == 3) printf("%d\n",0);
+	else {
+		printf("%d",(n*(n - 1)*(n - 2)*(n - 3)) / 24);//nC4
+	}
+
+	system("pause");
+	return 0;
+}
